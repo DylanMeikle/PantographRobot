e44f487051d7cefdb265ab3d227462f2ca85b1f6
DylanMeikle <dmeikle3@uwo.ca>
2023-03-19 15:44:45 -0400

    Milestone 2
    added code for claw and wrist and fixed the values for the legs

--- PentographRobot/PentographRobot.ino
+++ PentographRobot/PentographRobot.ino
@@ -18,27 +18,41 @@ boolean Time_Up_3 = false;
 unsigned int current_Millis;
 unsigned int previous_Millis;
 unsigned int Bot_Phase = 0;
-int pos = 0;
+int pos1 = 0;
+int pos2 = 0;
 int i = 0;
+int a;
+int b;
 
 //=============================================================
 //Values for Servos
 //Still need to test all 0 values
-const int Claw_Open = 0;    
-const int Claw_Closed = 0;
-const int Wrist_Max = 0;      
-const int Wrist_Straight = 0; //Wrist parallel with arm
-const int Wrist_Min = 0;
+const int Claw_Open = 490;    
+const int Claw_Closed = 1890;
+const int Wrist_Max = 1490;      
+const int Wrist_Straight = 800; //Wrist parallel with arm
+const int Wrist_Min = 290;
 //Leg1: 41
 //Leg2: 42
-const int Leg1_Max = 1;     //Legs holding the bot at maximum upright
-const int Leg2_Max = 2443;
+const int Leg1_Max = 400;     //Legs holding the bot at maximum upright
+const int Leg2_Max = 2100;
 const int Leg1_Min = 1282;  //Legs parallel with bot (not 180 to max)
 const int Leg2_Min = 1315;
 
+
+
 //=============================================================
-
-
+//=============================================================
+/* Notes:
+map(value, fromLow, fromHigh, toLow, toHigh)
+              
+value: the number to map.
+fromLow: the lower bound of the value’s current range.
+fromHigh: the upper bound of the value’s current range.
+toLow: the lower bound of the value’s target range.
+toHigh: the upper bound of the value’s target range.
+*/
+//============================================================
 
 void setup() 
 {
@@ -49,20 +63,29 @@ void setup()
   Bot.servoBegin("S3", CLAW);
   Bot.servoBegin("S4", WRIST);
   
+  //Legs(Leg1_Min, Leg2_Min);
+  //add code to have the claw start closed or open
 
+}
+void Legs(int Leg1, int Leg2){
+  int pos1 = map(Leg1 ,0, 4096, 0, 4096);
+  int pos2 = map(Leg2 ,0, 4096, 0, 4096);      
+        
+  Bot.ToPosition("S1", pos1);
+  Bot.ToPosition("S2", pos2); 
 }
 
 void loop() {
   
   current_Millis = millis();
-  if((current_Millis - previous_Millis) >= 1000){
+  if((current_Millis - previous_Millis) >= 2000){
     previous_Millis = current_Millis;
     Time_Up_1 = true;
   }
   
   if(Time_Up_1){
     Time_Up_1 = false;
-    /*
+    /* Order in which the Robot Phases Should Occur:
     0 - Stand up (Lowest servo can go)
     1 - Drive Forward until microcswitch1 detects edge
     2 - Based on distance from bar, calculate the angle the servos
@@ -82,48 +105,27 @@ void loop() {
     switch(Bot_Phase){
       case 0:
       {
-        /*
-        map(value, fromLow, fromHigh, toLow, toHigh)
-              
-        value: the number to map.
-        fromLow: the lower bound of the value’s current range.
-        fromHigh: the upper bound of the value’s current range.
-        toLow: the lower bound of the value’s target range.
-        toHigh: the upper bound of the value’s target range.
-        */
-        pos1 = map(Leg1_Max ,0, 4096, 0, 4096);
-        pos2 = map(Leg2_Max ,0, 4096, 0, 4096)        
+        Legs(Leg1_Max, Leg2_Max); 
+        Serial.printf("Legs Extended\n");
+        Bot_Phase = 1;
         
-        Bot.ToPosition("S1", pos1);
-        Bot.ToPosition("S2", pos2);  
-        //Serial.println(pos); 
-        Bot_Phase = 0;
         break;  
-        //42: min: 1315
-        // max: 2443
-        //41:min:1282
-        // max: 1
-        /* tester
-        max:611
-        min:1380
-        */        
+              
       }case 1:
       {
         //Straighten the wrist
         Bot.ToPosition("S4", Wrist_Straight);
-        Serial.printf("Wrist Straightened");
+        Serial.printf("Wrist Straightened\n");
         Bot_Phase = 2;
         break;
       }case 2:
       {
-      
-        if(i >= 5){
-          Bot.ToPosition("S3", Claw_Closed);
-          Serial.printf("Claw Closed");          
-          Bot_Phase = 0;
-        }
-        i++;
-        break;
+        
+        Bot.ToPosition("S3", Claw_Closed);
+        Serial.printf("Claw Closed\n");          
+        Bot_Phase = 0;
+        
+        
       }    
 
     }
